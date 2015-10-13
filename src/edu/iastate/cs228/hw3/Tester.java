b702133777e3fdedf163185d1fdcc0df8a135147
Charlie Steenhagen <gcs@iastate.edu>
2015-10-13 12:21:28 -0500

    no message

--- src/edu/iastate/cs228/hw3/Tester.java
+++ src/edu/iastate/cs228/hw3/Tester.java
@@ -4,15 +4,7 @@ import java.util.Random;
 
 public class Tester {
 	public static void main(String[] args){
-		int[] nums = new int[10];
-		Random rand = new Random();
-		for(int i=0;i<nums.length;i++){
-			nums[i] = rand.nextInt(9)+1;
-			for(int j =0;j<i;j++){
-				System.out.print(" ");
-			}
-			System.out.println(nums[i] + " ");
-		}
-		
+		//This is my tester class for Project 3. Here I will run unofficial tests.
+		//No JUnit tests will be in this class. This is just for my personal use.
 	}
 }
