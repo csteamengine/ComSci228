32b8c45c2cc4eaca524999faf37cfb86a5aeec22
csteamengine <gcs@iastate.edu>
2015-11-01 09:59:25 -0600

    Just tested the PrimeFactorization Constructor (long) with the tester class. Should Work, but something is wrong with the iterator or the add method... Not sure yet.

--- src/edu/iastate/cs228/hw3/Tester.java
+++ src/edu/iastate/cs228/hw3/Tester.java
@@ -6,9 +6,14 @@ public class Tester {
 	public static void main(String[] args){
 		//This is my tester class for Project 3. Here I will run unofficial tests.
 		//No JUnit tests will be in this class. This is just for my personal use.
-		PrimeFactorization pf = new PrimeFactorization(1000);
-		System.out.println(pf.toString());
-
-		
+		PrimeFactorization pf= new PrimeFactorization();
+		int n = 1000;
+		for(int i =2;i*i<n;i++){
+			while(pf.isPrime(i) && n%i==0){
+				System.out.println(n + "/" + i);
+				n=n/i;
+				System.out.println(n);
+			}
+		}
 	}
 }
